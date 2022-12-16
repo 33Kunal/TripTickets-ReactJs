5dec01c9d5cda5a894f1b657b68e6f30931fd061
Kunal <33kunalg@gmail.com>
2022-12-16 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ export default function App() {
  function handleTextareaChange(e) {
     setAnswer(e.target.value);
   }
+  return (
+    <>
+      <h2>City quiz</h2>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
 
 
   );
