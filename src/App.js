c0a3a79ea0b3618151271f2f6fd7a11fa4a51386
Kunal <33kunalg@gmail.com>
2022-12-21 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -33,6 +33,19 @@ export default function App() {
           disabled={status === 'submitting'}
         />
 
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima'
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
   );
 }
 
