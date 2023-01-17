6d96d7dda6ce1e11ab6ad503848ee7863546d3e6
Kunal <33kunalg@gmail.com>
2023-01-17 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -101,6 +101,11 @@ function handleFirstNameChange(e) {
      <Chat contact={to} />
    </div>
  )
+ const contacts = [
+  { name: 'Taylor', email: 'taylor@mail.com' },
+  { name: 'Alice', email: 'alice@mail.com' },
+  { name: 'Bob', email: 'bob@mail.com' }
+];
 }
 }
 
