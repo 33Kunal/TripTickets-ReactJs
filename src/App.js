9b5eb5bde4e5ce32e7cc86c8825db4b861301b16
Kunal <33kunalg@gmail.com>
2023-03-06 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -91,6 +91,24 @@ return (
         onDeleteTask={handleDeleteTask}
       />
   
-  
+  function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }];
+    }
+    case 'changed': {
+      return tasks.map(t => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+
   );
 
