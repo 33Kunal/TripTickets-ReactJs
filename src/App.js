520d6aa1b866e8acdde255962c7452b141791e0a
Kunal <33kunalg@gmail.com>
2023-03-08 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -126,6 +126,14 @@ const initialTasks = [
 default: {
   throw Error('Unknown action: ' + action.type);
 }
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
+];
+
+
 }
 }
 
