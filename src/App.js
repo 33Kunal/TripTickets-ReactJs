a5ebdaa87c45234b314c297ce13642f3695a5910
Kunal <33kunalg@gmail.com>
2023-03-08 08:47:50 +0530

    modified topics

--- src/App.js
+++ src/App.js
@@ -116,22 +116,7 @@ return (
       throw Error('Unknown action: ' + action.type);
     }
   }
-  let nextId = 3;
-const initialTasks = [
-  { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false }
-];
-}
-default: {
-  throw Error('Unknown action: ' + action.type);
-}
-let nextId = 3;
-const initialTasks = [
-  { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false }
-];
+  
 
 
 }
